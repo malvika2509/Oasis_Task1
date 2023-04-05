7dba89bef5d52e890e647b4bb191f8b584818344
malvika2509 <71529259+malvika2509@users.noreply.github.com>
2023-04-05 23:05:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  let [result, setResult] = useState("");
+
+  const handleClick = (e) => {
+    if (e.target.innerText == "x") {
+      setResult(result + e.target.value);
+    } else if (e.target.value == "root") {
+      result = Math.sqrt(result);
+      setResult(result);
+    } else {
+      setResult(result + e.target.innerText);
+    }
+  };
+
+  const del = () => {
+    setResult(result.slice(0, -1));
+  };
+  const clear = () => {
+    setResult("");
+  };
+  const calc = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (error) {
+      setResult("error");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="whole">
+      <h1 className="heading">Calculator 📏</h1>
+
+      <div className="container">
+        <form action="">
+          <input type="text" value={result} className="inp" />
+        </form>
+
+        <div className="calc">
+          <button onClick={clear} className="clear">
+            Clear
+          </button>
+          <button onClick={del} className="del">
+            del
+          </button>
+
+          <button onClick={handleClick}>&#x28;</button>
+          <button onClick={handleClick}>)</button>
+          <button onClick={handleClick}>1</button>
+          <button onClick={handleClick}>2</button>
+          <button onClick={handleClick}>3</button>
+          <button onClick={handleClick}>/</button>
+          <button value="root" onClick={handleClick}>
+            &#8730;
+          </button>
+          <button onClick={handleClick}>4</button>
+          <button onClick={handleClick}>5</button>
+          <button onClick={handleClick}>6</button>
+          <button onClick={handleClick}>%</button>
+          <button value="*" onClick={handleClick}>
+            x
+          </button>
+          <button onClick={handleClick}>7</button>
+          <button onClick={handleClick}>8</button>
+          <button onClick={handleClick}>9</button>
+          <button onClick={handleClick}>+</button>
+          <button onClick={handleClick}>-</button>
+          <button onClick={handleClick}>.</button>
+          <button onClick={handleClick}>0</button>
+          <button onClick={handleClick}>&#177;</button>
+          <button className="equal" onClick={calc}>
+            =
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
